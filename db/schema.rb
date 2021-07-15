ec6ea58658f1da60ca9a27d72aff3d185d441a42
VanillaHafer <craighafer1996@gmail.com>
2021-07-14 20:20:33 -0400

    Finished Chapter 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_212023) do
+ActiveRecord::Schema.define(version: 2021_07_14_214008) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
