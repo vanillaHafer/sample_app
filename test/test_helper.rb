6a851efb2f8d3d3165f6aa9c724f239701ec52e7
VanillaHafer <craighafer1996@gmail.com>
2021-07-16 21:15:30 -0400

    Implement basic login/logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,16 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
 
   # Add more helper methods to be used by all tests here...
 end
