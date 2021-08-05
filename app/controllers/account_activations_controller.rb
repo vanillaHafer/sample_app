0ab08f3a528a19171d1e80bd53f238d77121298d
VanillaHafer <craighafer1996@gmail.com>
2021-08-05 18:29:15 -0400

    Fix typo in flash key

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,7 +5,7 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:sucess] = "Account activated!"
+      flash[:success] = "Account activated!"
       redirect_to user
     else
       flash[:danger] = "Invalid activation link"
