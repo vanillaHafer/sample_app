c2424530a41e4784c962fdd545893452f8a5b664
VanillaHafer <craighafer1996@gmail.com>
2021-08-13 14:30:42 -0400

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+ 
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -79,6 +80,8 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+
+
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
